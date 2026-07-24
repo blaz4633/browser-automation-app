d1e4eb7737efddef41a9822f854170e5a0a8de53
Francis B. Foley <blaz4633@hotmail.com>
2026-07-24 07:46:06 -0400

    feat: add clerk authentication

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,20 @@
 import { Geist, Geist_Mono } from "next/font/google"
+import {
+  ClerkProvider,
+  Show,
+  SignInButton,
+  SignUpButton,
+  UserButton,
+} from "@clerk/nextjs"
+import { shadcn } from "@clerk/ui/themes"
 
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
 import { Toaster } from "@/components/ui/sonner"
-import { cn } from "@/lib/utils";
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
 
-const geist = Geist({subsets:['latin'],variable:'--font-sans'})
+const geist = Geist({ subsets: ["latin"], variable: "--font-sans" })
 
 const fontMono = Geist_Mono({
   subsets: ["latin"],
@@ -21,11 +30,35 @@ export default function RootLayout({
     <html
       lang="en"
       suppressHydrationWarning
-      className={cn("antialiased", fontMono.variable, "font-sans", geist.variable)}
+      className={cn(
+        "antialiased",
+        fontMono.variable,
+        "font-sans",
+        geist.variable
+      )}
     >
       <body>
         <ThemeProvider>
-          {children}
+          <ClerkProvider
+            appearance={{ theme: shadcn }}
+            signInUrl="/sign-in"
+            signUpUrl="/sign-up"
+          >
+            <header className="flex h-16 items-center justify-end gap-4 p-4">
+              <Show when="signed-out">
+                <SignInButton mode="redirect" />
+                <SignUpButton mode="redirect">
+                  <Button className="h-10 cursor-pointer rounded-full bg-[#6c47ff] px-4 text-sm font-medium text-white sm:h-12 sm:px-5 sm:text-base">
+                    Sign Up
+                  </Button>
+                </SignUpButton>
+              </Show>
+              <Show when="signed-in">
+                <UserButton />
+              </Show>
+            </header>
+            {children}
+          </ClerkProvider>
           <Toaster />
         </ThemeProvider>
       </body>
