14299bcac9783b9f721ddd7bdaac725b696bd912
Francis B. Foley <blaz4633@hotmail.com>
2026-07-31 16:40:44 -0400

    feat: setup react-flow with basic example in new canvas component

--- features/workflows/components/workflow-shell.tsx
+++ features/workflows/components/workflow-shell.tsx
@@ -6,6 +6,7 @@ import {
   ResizablePanelGroup,
 } from "@/components/ui/resizable"
 
+import { Canvas } from "./canvas"
 import { RightSidebar } from "./right-sidebar"
 
 export function WorkflowShell({ workflowId }: { workflowId: string }) {
@@ -18,7 +19,7 @@ export function WorkflowShell({ workflowId }: { workflowId: string }) {
       <ResizablePanel minSize="30rem">
         <ResizablePanelGroup orientation="vertical" className="size-full">
           <ResizablePanel minSize="18rem">
-            <div className="size-full p-4">Canvas</div>
+            <Canvas />
           </ResizablePanel>
           <ResizableHandle />
           <ResizablePanel defaultSize="8rem" minSize="6rem">
